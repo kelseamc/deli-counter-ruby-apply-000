5dd899f53fbd9ed82e18ed41827ff13b6fc1f020
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-24 15:54:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,22 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, new_customer)
-  katz_deli << new_customer
-  puts "Welcome, #{new_customer}. You are number #{(katz_deli.length)} in line."
+# def take_a_number(katz_deli, new_customer)
+#   katz_deli << new_customer
+#   puts "Welcome, #{new_customer}. You are number #{(katz_deli.length)} in line."
+# end
+$count=0 
+katz_deli = [] 
+def take_a_number(katz_deli)
+  $count +=1
+  katz_deli << $count 
+  puts katz_deli
+  puts "Welcome, You are number #{$count} in line."
 end
+
+take_a_number(katz_deli)
+take_a_number(katz_deli)
+take_a_number(katz_deli)
 
 def now_serving(katz_deli)
   if katz_deli.empty?
